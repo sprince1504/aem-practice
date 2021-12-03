a00178d7ea06e7f2933b4f8dad244dbe7932d1a4
Prince Shivhare <prince.shivhare@lntinfotech.com>
2021-12-03 15:43:06 +0530

    Factory configuration aem.

---
+++ core/src/main/java/com/mysite/core/services/FactoryConfig.java
@@ -0,0 +1,6 @@
+package com.mysite.core.services;
+
+public interface FactoryConfig {
+    String endPointUrl();
+    String siteIdentifier();
+}
---
+++ core/src/main/java/com/mysite/core/services/FactoryConfigOCD.java
@@ -0,0 +1,12 @@
+package com.mysite.core.services;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(name = "Factory Configuration Example", description = "Factory Configuration")
+public @interface FactoryConfigOCD {
+    @AttributeDefinition(name = "Endpoint to connect to API", description = "EndPoint Connection")
+    String apiEndPoint();
+    @AttributeDefinition(name = "Site Identifier", description = "Identify the site")
+    String siteIdentifier();
+}
